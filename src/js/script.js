68a727e71e174b607c8e7b316537f18f32cb7063
Victor Luiz Flor <victorflor56@gmail.com>
2023-02-28 14:25:07 -0400

    estilizando hard skills e arrumando icones

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const popUP = document.querySelector(".popUP")
+const imagem = document.querySelector(".popUP img");
+const texto = document.querySelector(".popUp p")
+
+popUP.onmouseover = function (){
+    texto.style.display = on;
+}
