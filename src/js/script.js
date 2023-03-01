352aeda32ff2a1d9a9b93c1948efc525464fce02
Victor Luiz Flor <victorflor56@gmail.com>
2023-02-28 22:07:41 -0400

    alguns ajustes na barra de navegação

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const popUP = document.querySelector(".popUP")
-const imagem = document.querySelector(".popUP img");
-const texto = document.querySelector(".popUp p")
-
-popUP.onmouseover = function (){
-    texto.style.display = on;
-}
